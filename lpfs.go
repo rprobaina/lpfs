85ba46ba22c36fd63cc16c6c5f045c9b5db33ed4
Ricardo Robaina <rprobaina@gmail.com>
2022-07-14 14:56:24 -0300

    lpfs: Bug fix.

--- lpfs.go
+++ lpfs.go
@@ -8,59 +8,59 @@ import (
 )
 
 var (
-	PROCDIR          string = "/proc"
-	PROCDIR_LOADAVG  string = "/proc/loadavg"
-	PROCDIR_MEMINFO  string = "/proc/meminfo"
-	PROCDIR_SWAPS    string = "/proc/swaps"
-	STATUSFILE       string = "stat"
+	PROCDIR         string = "/proc"
+	PROCDIR_LOADAVG string = "/proc/loadavg"
+	PROCDIR_MEMINFO string = "/proc/meminfo"
+	PROCDIR_SWAPS   string = "/proc/swaps"
+	STATUSFILE      string = "stat"
 )
 
 //	GetLoadAverage1 returns the load average over the last minute.
-func GetLoadAverage1() (float32, error) {
+func GetLoadAverage1() (float64, error) {
 	dat, err := os.ReadFile(PROCDIR_LOADAVG)
 	if err != nil {
-		return 0.0, err;
+		return 0.0, err
 	}
 
 	dat_s := strings.Split(string(dat), " ")[0]
 
 	lavg, err := strconv.ParseFloat(dat_s, 32)
 	if err != nil {
-		return 0.0, err;
+		return 0.0, err
 	}
 
-	return lavg1, nil
+	return lavg, nil
 }
 
-//	GetLoadAverage5 returns the load average over the last minute.
-func GetLoadAverage5() (float32, error) {
+//	GetLoadAverage5 returns the load average over the last 5 minutes.
+func GetLoadAverage5() (float64, error) {
 	dat, err := os.ReadFile(PROCDIR_LOADAVG)
 	if err != nil {
-		return 0.0, err;
+		return 0.0, err
 	}
 
 	dat_s := strings.Split(string(dat), " ")[1]
 
 	lavg, err := strconv.ParseFloat(dat_s, 32)
 	if err != nil {
-		return 0.0, err;
+		return 0.0, err
 	}
 
 	return lavg, nil
 }
 
-//	GetLoadAverage15 returns the load average over the last minute.
-func GetLoadAverage15() (float32, error) {
+//	GetLoadAverage15 returns the load average over the last 15 minutes.
+func GetLoadAverage15() (float64, error) {
 	dat, err := os.ReadFile(PROCDIR_LOADAVG)
 	if err != nil {
-		return 0.0, err;
+		return 0.0, err
 	}
 
 	dat_s := strings.Split(string(dat), " ")[2]
 
 	lavg, err := strconv.ParseFloat(dat_s, 32)
 	if err != nil {
-		return 0.0, err;
+		return 0.0, err
 	}
 
 	return lavg, nil
