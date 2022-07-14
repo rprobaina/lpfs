eb49d813c0ae6c01062a809bc0a36a04af9707e3
Ricardo Robaina <rprobaina@gmail.com>
2022-07-14 14:47:27 -0300

    lpfs: Adding GetLoadAverage5 and GetLoadAverage15.

--- lpfs.go
+++ lpfs.go
@@ -8,8 +8,6 @@ import (
 )
 
 var (
-	DEFAULT_SAMPLES  int    = 1
-	DEFAULT_INTERVAL int    = 1
 	PROCDIR          string = "/proc"
 	PROCDIR_LOADAVG  string = "/proc/loadavg"
 	PROCDIR_MEMINFO  string = "/proc/meminfo"
@@ -18,19 +16,52 @@ var (
 )
 
 //	GetLoadAverage1 returns the load average over the last minute.
-func GetLoadAverage1() float64 {
-
+func GetLoadAverage1() (float32, error) {
 	dat, err := os.ReadFile(PROCDIR_LOADAVG)
 	if err != nil {
-		// TODO
+		return 0.0, err;
 	}
 
 	dat_s := strings.Split(string(dat), " ")[0]
 
-	lavg1, err := strconv.ParseFloat(dat_s, 64)
+	lavg, err := strconv.ParseFloat(dat_s, 32)
 	if err != nil {
-		// TODO
+		return 0.0, err;
 	}
 
-	return lavg1
+	return lavg1, nil
+}
+
+//	GetLoadAverage5 returns the load average over the last minute.
+func GetLoadAverage5() (float32, error) {
+	dat, err := os.ReadFile(PROCDIR_LOADAVG)
+	if err != nil {
+		return 0.0, err;
+	}
+
+	dat_s := strings.Split(string(dat), " ")[1]
+
+	lavg, err := strconv.ParseFloat(dat_s, 32)
+	if err != nil {
+		return 0.0, err;
+	}
+
+	return lavg, nil
+}
+
+//	GetLoadAverage15 returns the load average over the last minute.
+func GetLoadAverage15() (float32, error) {
+	dat, err := os.ReadFile(PROCDIR_LOADAVG)
+	if err != nil {
+		return 0.0, err;
+	}
+
+	dat_s := strings.Split(string(dat), " ")[2]
+
+	lavg, err := strconv.ParseFloat(dat_s, 32)
+	if err != nil {
+		return 0.0, err;
+	}
+
+	return lavg, nil
 }
